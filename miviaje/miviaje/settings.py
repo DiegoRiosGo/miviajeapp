d97b9dc69aaf35ef79df6253e476a9afbb3788ec
DiegoRiosGo <die.rios@duocuc.cl>
2024-06-20 18:32:14 -0400

    aer

--- miviaje/miviaje/settings.py
+++ miviaje/miviaje/settings.py
@@ -14,12 +14,6 @@ from pathlib import Path
 import dj_database_url
 import django_heroku
 
-
-
-# Configurar Django App para usar la configuración de Heroku
-django_heroku.settings(locals())
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,8 +22,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # La URL que se utilizará cuando se haga referencia a archivos estáticos (desde donde se entregarán)
 STATIC_URL = '/static/'
-
-
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
@@ -40,18 +32,16 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-vbl))*9)h+*qul#s3@asqsmexyd^sc(f%#l%=(bb^b^!xal-5_'
-
+# SECRET_KEY = 'django-insecure-vbl))*9)h+*qul#s3@asqsmexyd^sc(f%#l%=(bb^b^!xal-5_'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-vbl))*9)h+*qul#s3@asqsmexyd^sc(f%#l%=(bb^b^!xal-5_')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -92,18 +82,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'miviaje.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tu_nombre_de_base_de_datos',
+        'USER': 'tu_usuario',
+        'PASSWORD': 'tu_contraseña',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Agregar la configuración de la base de datos usando dj-database-url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -119,10 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -131,11 +126,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Whitenoise settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configurar Django App para usar la configuración de Heroku
+django_heroku.settings(locals())
